47e3d1b4d46fa3e6643939a4397ad0df0da87ece
Matthias Watermann <support@mwat.de>
2024-06-26 19:35:10 +0200

    + initial checkin;

---
+++ codeerror_test.go
@@ -0,0 +1,113 @@
+/*
+Copyright © 2024  M.Watermann, 10247 Berlin, Germany
+
+	    All rights reserved
+	EMail : <support@mwat.de>
+*/
+package codeerror
+
+import (
+	"errors"
+	"fmt"
+	"testing"
+)
+
+//lint:file-ignore ST1017 - I prefer Yoda conditions
+
+func TestErrCodeLocation_Error(t *testing.T) {
+	cl1 := fmt.Errorf("dummy")
+	e1 := fmt.Errorf("some first error")
+	if nil != e1 {
+		cl1 = CodeError(e1, 2)
+	}
+	w1 := fmt.Sprintf("%s", cl1) // use Stringer interface
+
+	cl2 := fmt.Errorf("dummy")
+	e2 := fmt.Errorf("some second error")
+	if nil != e2 {
+		cl2 = CodeError(e2, 2)
+	}
+	w2 := fmt.Sprintf("%s", cl2)
+
+	tests := []struct {
+		name   string
+		fields error // i.e. ErrCodeLocation
+		want   string
+	}{
+		{"1", cl1, w1},
+		{"2", cl2, w2},
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			cl := tt.fields
+			if got := cl.Error(); got != tt.want {
+				t.Errorf("%q: ErrCodeLocation.Error() =\n%q,\nwant %q",
+					tt.name, got, tt.want)
+			}
+		})
+	}
+} // testErrCodeLocation()
+
+func TestErrCodeLocation_String(t *testing.T) {
+	var (
+		cl1, cl2 ErrCodeLocation
+	)
+	e1 := fmt.Errorf("some first error")
+	cl11 := CodeError(e1, 1)
+	if errors.Is(cl11, cl1) {
+		cl1 = cl11.(ErrCodeLocation)
+	}
+	w1 := fmt.Sprintf(strPattern, cl1.File, cl1.Line, cl1.Function)
+
+	e2 := fmt.Errorf("some second error")
+	cl22 := CodeError(e2, 1)
+	if errors.Is(cl22, cl2) {
+		cl2 = cl22.(ErrCodeLocation)
+	}
+	w2 := fmt.Sprintf(strPattern, cl2.File, cl2.Line, cl2.Function)
+
+	tests := []struct {
+		name   string
+		fields ErrCodeLocation
+		want   string
+	}{
+		{"1", cl1, w1},
+		{"2", cl2, w2},
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			cl := tt.fields
+			if got := cl.String(); got != tt.want {
+				t.Errorf("%q: ErrCodeLocation.String() =\n%q,\nwant %q",
+					tt.name, got, tt.want)
+			}
+		})
+	}
+} // TestErrCodeLocation_String()
+
+func TestErrCodeLocation_Unwrap(t *testing.T) {
+	e1 := fmt.Errorf("some first error")
+	cl1 := CodeError(e1, 1)
+
+	tests := []struct {
+		name    string
+		fields  error // i.e. ErrCodeLocation
+		wantErr error
+	}{
+		{"1", cl1, e1},
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			cl := tt.fields.(*ErrCodeLocation)
+			if got := cl.Unwrap(); got != tt.wantErr {
+				t.Errorf("%q: ErrCodeLocation.Unwrap() error = %v, wantErr %v",
+					tt.name, got, tt.wantErr)
+			}
+		})
+	}
+} // TestErrCodeLocation_Unwrap()
+
+/* _EoF_ */
